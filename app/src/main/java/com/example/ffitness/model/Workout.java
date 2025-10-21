760aa053845b438ec45493aeadda4f5eae4fd4b0
Trang Vu <144322664+vulinhtrang2305@users.noreply.github.com>
2025-10-21 10:08:41 +0700

    feat: workouts

---
+++ app/src/main/java/com/example/ffitness/model/Workout.java
@@ -0,0 +1,33 @@
+package com.example.ffitness.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Workout implements Serializable {
+    @SerializedName("_id")
+    private String id;
+
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("image")
+    private String image;
+
+    @SerializedName("isPublic")
+    private Boolean isPublic;
+
+    @SerializedName("user")
+    private User user;
+
+    @SerializedName("exercises")
+    private List<WorkoutSession> exercises;
+}
