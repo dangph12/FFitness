e5256b2d9c4245bae1fbf8c983431c6c03311a28
ducndahe181048 <144119087+ducndahe181048@users.noreply.github.com>
2025-11-10 21:52:42 +0700

    chore: delete calculateDaysSince method in profile page

--- app/src/main/java/com/example/ffitness/ui/ProfileFragment.java
+++ app/src/main/java/com/example/ffitness/ui/ProfileFragment.java
@@ -242,15 +242,4 @@ public class ProfileFragment extends Fragment {
         }
     }
 
-    private long calculateDaysSince(Date createdDate) {
-        if (createdDate == null) return 0;
-
-        Date today = new Date();
-
-        long diffInMillis = today.getTime() - createdDate.getTime();
-        long days = diffInMillis / (1000 * 60 * 60 * 24);
-
-        return days;
-    }
-
 }
